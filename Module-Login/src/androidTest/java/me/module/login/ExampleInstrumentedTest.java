67bdd5ae54df184e69d1461156ee81f1077a613f
DesignQu <250533855@qq.com>
2018-08-29 15:12:46 +0800

    完善配置文件

--- Module-Login/src/androidTest/java/me/module/login/ExampleInstrumentedTest.java
+++ Module-Login/src/androidTest/java/me/module/login/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package me.component.sdk;
+package me.module.login;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("me.component.sdk.test", appContext.getPackageName());
+        assertEquals("me.module.login.test", appContext.getPackageName());
     }
 }
--- Module-Login/src/test/java/me/module/login/ExampleUnitTest.java
+++ Module-Login/src/test/java/me/module/login/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package me.component.sdk;
+package me.module.login;
 
 import org.junit.Test;
 
