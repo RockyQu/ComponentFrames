682b00e1011513306254044a64e858cfd90f904d
DesignQu <250533855@qq.com>
2018-09-05 18:07:31 +0800

    添加了一些 Router 必要的类

--- Router-compiler/src/main/java/me/router/compiler/RouterProcessor.java
+++ Router-compiler/src/main/java/me/router/compiler/RouterProcessor.java
@@ -3,10 +3,16 @@ package me.router.compiler;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.TypeElement;
 
 public class RouterProcessor extends AbstractProcessor {
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnvironment) {
+        super.init(processingEnvironment);
+    }
 
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
