263796205757806e7e332730e35e47da97639616
Qu <250533855@qq.com>
2018-12-26 17:36:06 +0800

    arouter-api 1.3.1 → 1.4.1

--- ComponentSDK/src/main/java/me/component/sdk/utils/RouterUtils.java
+++ ComponentSDK/src/main/java/me/component/sdk/utils/RouterUtils.java
@@ -1,5 +1,18 @@
 package me.component.sdk.utils;
 
+import android.content.Context;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+
 public class RouterUtils {
 
+    /**
+     * 页面跳转
+     *
+     * @param context
+     * @param path
+     */
+    public static void navigation(Context context, String path) {
+        ARouter.getInstance().build(path).navigation(context);
+    }
 }
